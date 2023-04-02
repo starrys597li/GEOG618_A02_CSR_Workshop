80af117a9aa6364475c3518fb9e80dd54393a50e
Siyu Li <64757199+starrys597li@users.noreply.github.com>
2023-04-02 15:39:00 -0400

    Update csr_workshop.R

--- csr_workshop.R
+++ csr_workshop.R
@@ -15,14 +15,14 @@ library(maptools)
 library(sp)
 
 ### 3. Load the point pattern data (shapefile with projection)
-points <- readShapePoints("D:/crime/c2021GTArobberyoccur.shp")
+points <- readShapePoints("D:/618shp/c2021GTArobberyoccur.shp")
 # read the shp file
 pp <- as.ppp(points)
 # convert the shp file into a a point pattern dataset
 unitname(pp) <- "meter"
 # set the unit of the point pattern dataset be meter
 
-############# Ripley's K-function ##########
+################## Ripley's K-function ###############
 
 ### a. Create a spatial window to define the spatial extent of the study area
 bbox <- bbox(points)
@@ -32,33 +32,39 @@ win <- owin(xrange = bbox[1,], yrange = bbox[2,])
 Kfun_test <- Kest(pp)
 
 ### c. Plot the K-function
-plot(Kfun_test, main = "Ripley's K-function - 2021 GTA robbery occurrences")
+plot(Kfun_test, main = "Ripley's K-function")
 
 ### d. Calculate the confidence envelope. This function generates a simulated 
 ### envelope of K(r) under CSR, which can be used to assess whether the observed 
 ### K-function deviates significantly from CSR.
-envK <- envelope(pp, Kest, nsim=100)
+envK <- envelope(pp, Kest, nsim = 999)
 
 ### e. Plot the confidence envelope and the observed K-function
-plot(envK, main = "Ripley's K-function - 2021 GTA robbery occurrences")
+plot(envK, main = "Ripley's K-function (nsim=999)")
 
-########## Besag's L-function ##########
+############### Besag's L-function ###############
 
-### a. Compute the observed and theoretical values of Besag's L-function using the Lest function
-Lemp <- Lest(pp)
-Ltheo <- envelope(pp, Lest, nsim = 99)
+### a. Calculate the Besag's L-function
+L <- envelope(pp, Lest, nsim = 99)
 
-### b. Plot the observed and theoretical values of Besag's L-function
-plot(Lemp, main = "Empirical and Theoretical L-function - 2021 GTA robbery occurrences", xlab = "r", ylab = "L(r)", col = "red")
-plot(Ltheo, add = TRUE, col = "blue")
-legend("bottomright", c("Empirical", "Theoretical"), col = c("red", "blue"), lty = 1)
+# Plot the L-function and the confidence interval
+plot(L, main = "Besag's L-function", xlab = "Distance", ylab = "L(r)")
+lines(L$theo, col = "red", lty = 2) # add the theoretical L-function as a dashed line
+lines(L$upper, col = "black", lty = 2) # add the upper bound of the confidence interval as a dashed line
+lines(L$lower, col = "black", lty = 2) # add the lower bound of the confidence interval as a dashed line
 
-########## Besag's L-function ##########
+############### Clark and Evans' R-function ###############
 ### a. Calculate the Clark and Evans' R-function
 ce <- clarkevans.test(pp)
-
-
+ce
 ### b. Print the results to determine whether the point pattern is CSR or not. 
 ### The output will show the observed and expected values of R at each distance, 
 ### along with the p-value of the test.
-ce
+ce_test_p_value <- ce$p.value
+
+### c. Interpretation of results
+if(ce_test_p_value >= 0.05){
+  print("The observed spatial pattern is consistent with CSR.")
+} else {
+  print("The observed spatial pattern is not consistent with CSR.")
+}
